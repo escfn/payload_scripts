ddd892e6f7725adce92fed6b4638ba7f5391dc45
soC <socrates.escfn@gmail.com>
2017-02-26 22:13:33 -0500

    Update Terminal.s

--- Terminal.s
+++ Terminal.s
@@ -1,11 +1,11 @@
 #################################
 #                     .g8888bgd #
 #                   .dP       M #
-# ,pP"Ybd  ,pW8Wq.  dM        ; #
+# ,pP"Ybd  ,pW8Wq.  dM        | #
 # 8I      6W     Wb MM          #
 #  YMMMa. 8M     M8 MM.         #
 # L.   I8 YA.   ,A9  Mb.     ,  #
-# M9mmmP;  .Ybmd9.    ..bmmmd.  #
+# M9mmmP|  .Ybmd9.    ..bmmmd.  #
 #################################
 
 .section .data 
